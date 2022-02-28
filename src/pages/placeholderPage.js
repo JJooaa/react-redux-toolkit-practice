d8956e95a9c779c2d502818a73c66de4094b2e51
JJooaa <leitola.joa@gmail.com>
2022-02-28 09:38:57 +0200

    update

--- src/pages/placeholderPage.js
+++ src/pages/placeholderPage.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchPosts } from "../features/posts/postsSlice";
+import { changePage } from "../features/page/pageSlice";
 
 const PlaceholderPage = () => {
     const dispatch = useDispatch();
 
-    const posts = useSelector((state) => state.posts.posts);
-
     const fetchStatus = useSelector((state) => state.posts.status);
 
+    const errorStatus = useSelector((state) => state.posts.error);
     useEffect(() => {
         if (fetchStatus === "idle") {
             dispatch(fetchPosts());
@@ -17,11 +17,23 @@ const PlaceholderPage = () => {
 
     let element;
 
-    if (fetchStatus.loading) {
+    if (fetchStatus === "loading") {
         element = <p>Loading...</p>;
+    } else if (fetchStatus === "succeeded") {
+        element = <div>Successul</div>;
+    } else if (fetchStatus === "failed") {
+        element = <div>{errorStatus}</div>;
     }
 
-    return <section>placeholderpage</section>;
+    return (
+        <section>
+            <button onClick={() => dispatch(changePage("counter-page"))}>
+                go to counter page
+            </button>
+            <p>placeholder-page</p>
+            {element}
+        </section>
+    );
 };
 
 export default PlaceholderPage;
